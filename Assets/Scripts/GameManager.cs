5d47e175a9d2908d37dc20f84154245e4e7e5c4f
Blake <mrblakeman1@gmail.com>
2022-10-17 21:24:26 -0400

    game fundamentals completed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //directly assigned in the inspector
+    [SerializeField] VRPlayer player;
+    [SerializeField] List<SearchObject> objectsToFind; 
+    [SerializeField] List<HidingLocation> hidingLocations; 
+    [SerializeField] List<TargetLocation> targetLocations;
+    [SerializeField] Transform startLocation;
+    [SerializeField] StartButton startButton;
+    
+    public enum GAME_STATE { IDLE, STARTED, FINISHED }
+    public GAME_STATE gameState = GAME_STATE.IDLE;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        startButton.buttonPressed += startPressed;
+        yield return new WaitForSeconds(3.0f);
+        StartCoroutine(resetGame());
+    }
+    
+    void startPressed(VRHand hand)
+    {
+        if (gameState == GAME_STATE.IDLE)
+        {
+            startGame();
+        }
+        else if(gameState == GAME_STATE.STARTED)
+        {
+
+            bool gameFinished = true;
+            foreach(TargetLocation t in targetLocations)
+            {
+                if (!t.isFound)
+                {
+                    gameFinished = false;
+                    break;
+                }
+            }
+            if (gameFinished)
+            {
+                //end game
+                endGame();
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void startGame()
+    {
+        List<HidingLocation> hidingTemp = new List<HidingLocation>(hidingLocations);
+        foreach(SearchObject so in objectsToFind)
+        {
+            //choose a hiding location randomly that isn't already chosen
+            int loc = Random.Range(0, hidingTemp.Count);
+            so.transform.position = hidingTemp[loc].transform.position;
+            hidingTemp.RemoveAt(loc);
+            so.gameObject.SetActive(true);
+        }
+        gameState = GAME_STATE.STARTED;
+    }
+
+    public void endGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public IEnumerator resetGame()
+    {
+        yield return new WaitForSeconds(1.0f);
+        player.doTeleport(startLocation.position);
+        yield return new WaitForSeconds(1.0f);
+
+    }
+}
---
+++ Assets/Scripts/TargetLocation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TargetLocation : MonoBehaviour
+{
+    [SerializeField] Color foundColor;
+    [SerializeField] Color notFoundColor;
+    [SerializeField] GameObject targetCollider;
+    public SearchObject target;
+    public bool isFound;
+    public TMP_Text label3d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        label3d.text = target.label;
+        targetCollider.GetComponent<Renderer>().material.color = notFoundColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.attachedRigidbody;
+        if (rb == null) return;
+        SearchObject so = rb.GetComponent<SearchObject>();
+        if (so == null) return;
+        if(so == target)
+        {
+            isFound = true;
+            targetCollider.GetComponent<Renderer>().material.color = foundColor;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody rb = other.attachedRigidbody;
+        if (rb == null) return;
+        SearchObject so = rb.GetComponent<SearchObject>();
+        if (so == null) return;
+        if(so == target)
+        {
+            isFound = false;
+            targetCollider.GetComponent<Renderer>().material.color = notFoundColor;
+        }
+    }
+}
